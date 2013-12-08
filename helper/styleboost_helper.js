9736087d5b616d7b9ef042d2a6185779defbfcd0
Jelle De Loecker <jelle@kipdola.be>
2013-12-08 19:35:12 +0200

    Add own helper

---
+++ helper/styleboost_helper.js
@@ -0,0 +1,44 @@
+module.exports = function styleboostHelpers(hawkejs) {
+	
+	// References
+	var helpers = hawkejs.helpers,
+	    sb      = helpers.sb = {};
+
+	/**
+	 * Create a bootstrap menu
+	 *
+	 * @author   Jelle De Loecker   <jelle@codedor.be>
+	 * @since    0.0.1
+	 * @version  0.0.1
+	 *
+	 * @param    {String}    menuName    The name of the menu to get
+	 * @param    {Object}    options     Extra options
+	 */
+	sb.menu = function menu_get(modelName, options) {
+
+		if (typeof options !== 'object') {
+			options = {};
+		} else {
+			// clone the options object
+			options = JSON.parse(JSON.stringify(options));
+		}
+
+		// The html the root should be opened with
+		options.rootOpen = '<ul class="nav navbar-nav side-nav">';
+		options.rootClose = '</ul>';
+
+		// The html children should be opened with
+		options.childrenOpen = '<ul class="children">';
+		options.childrenClose = '</ul>';
+
+		options.sectionOpen = '<li class="section">';
+		options.sectionClose = '</li>';
+
+		options.sectionContent = {'class': 'section-toggle'};
+
+		options.singleOpen = '<li>';
+		options.singleClose = '</li>';
+
+		this.menu.get(modelName, options);
+	};
+};
