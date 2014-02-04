30540cd69b5548541cee44abaa0ad6d4a048088a
Kjell Keisse <kjell@codedor.be>
2014-02-04 11:28:06 +0100

    gave menu helper admin option for codedor logo

--- helper/styleboost_helper.js
+++ helper/styleboost_helper.js
@@ -23,9 +23,16 @@ module.exports = function styleboostHelpers(hawkejs) {
 			options = JSON.parse(JSON.stringify(options));
 		}
 
-		// The html the root should be opened with
-		options.rootOpen = '<ul class="nav navbar-nav side-nav">';
+		// The html the root should be opened with		
+		if(options.admin === true){
+			options.rootOpen = '<ul class="nav navbar-nav side-nav" id="admin-sidebar">';
+		} else {
+			options.rootOpen = '<ul class="nav navbar-nav side-nav">';
+			
+		}
+		
 		options.rootClose = '</ul>';
+		
 
 		// The html children should be opened with
 		options.childrenOpen = '<ul class="children">';
