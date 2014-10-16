1500921e80289483deb26b7d04b622af28bd37c7
Kjell Keisse <kjell@codedor.be>
2014-10-16 15:17:53 +0200

    added medium-editor1.9.4

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,5 @@
-var async     = alchemy.use('async'),
-    path      = alchemy.use('path'),
-    fs        = alchemy.use('fs'),
-
+var libpath   = alchemy.use('path'),
     // Where the original files are located
-    source    = path.resolve(__dirname, 'source');
+    source    = libpath.resolve(__dirname, 'source');
 
 alchemy.addStylesheetDirectory(source);
